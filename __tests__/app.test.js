a6c5b7d37da6c96b35f5465235a885b325941c87
Andrzej Łuszcz <endriu1297@gmail.com>
2020-11-23 16:42:47 +0100

    add test for logIn route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,19 @@
 const request = require('supertest');
 const app = require('../app');
 
-it('works', async () => {
-  const response = await request(app).get('/');
-
+function checkResponse(response) {
   expect(response.status).toEqual(200);
   expect(response.header['content-type']).toEqual('text/html; charset=utf-8');
+}
+
+it('It should work for method get on path /.', async () => {
+  const response = await request(app).get('/');
+
+  checkResponse(response);
+});
+
+it('It should work for method get on path /logIn.', async () => {
+  const response = await request(app).get('/logIn');
+
+  checkResponse(response);
 });
