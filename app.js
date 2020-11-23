2bb26de7d26d615f1690c1b4b271c7aac6f80001
Andrzej Łuszcz <endriu1297@gmail.com>
2020-11-23 16:42:21 +0100

    add new routes with get method and view for this route. Add this all to app

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var config = require('./config');
 
 var indexRouter = require('./routes/index');
+var logInRouter = require('./routes/logIn');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(
 );
 
 app.use('/', indexRouter);
+app.use('/logIn', logInRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
