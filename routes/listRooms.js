d3ad2d55e6fef0e2c9544c05a1fcbc1fcb82483c
Andrzej Łuszcz <endriu1297@gmail.com>
2020-11-24 18:33:40 +0100

    send data to template adn generate list of rooms from db

--- routes/listRooms.js
+++ routes/listRooms.js
@@ -3,16 +3,13 @@ var router = express.Router();
 const Room = require('../models/room');
 
 router.get('/', function (req, res, next) {
-  //   const findRoom = Room.find()
-  //     .sort({
-  //       dateAdd: -1,
-  //     })
-  //     .limit(4);
+  const findRoom = Room.find().sort({
+    dateAdd: -1,
+  });
 
-  //   findRoom.exec((err, data) => {
-  //     res.render('index', { role: req.session.role, data });
-  //   });
-  res.render('listRooms');
+  findRoom.exec((err, data) => {
+    res.render('listRooms', { role: req.session.role, data });
+  });
 });
 
 module.exports = router;
