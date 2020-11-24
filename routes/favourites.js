9a5bf88d6b3236aae134a53b333eec788535bba7
Andrzej Łuszcz <endriu1297@gmail.com>
2020-11-25 00:10:01 +0100

    download from db list rooms

--- routes/favourites.js
+++ routes/favourites.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
-const Favourite = require('../models/favourites');
+const Room = require('../models/room');
 
 router.get('/', function (req, res) {
-  res.render('favourites');
+  const findRoom = Room.find().sort({
+    dateAdd: -1,
+  });
+
+  findRoom.exec((err, data) => {
+    res.render('favourites', { role: req.session.role, data });
+  });
 });
 
 module.exports = router;
