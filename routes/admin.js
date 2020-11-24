029b71390b2f91f2ec38714c77956242af3d0257
Andrzej Łuszcz <endriu1297@gmail.com>
2020-11-24 01:20:11 +0100

    show list of users from db in admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/user');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('admin');
-  //   res.render('admin');
+  const findUser = User.find().sort({
+    login: -1,
+  });
 
-  //   if (req.session.role) {
-  //     console.log('Wywołałem się');
-  //     res.render('admin', { role: req.session.role });
-  //   } else {
-  //     res.render('admin');
-  //   }
+  findUser.exec((err, data) => {
+    console.log(data);
+
+    res.render('admin', { data });
+  });
 });
 
 module.exports = router;
--- routes/logIn.js
+++ routes/logIn.js
@@ -8,7 +8,6 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
-  console.log(req.body);
   const loginForm = req.body.login;
   const passwordForm = req.body.password;
 
