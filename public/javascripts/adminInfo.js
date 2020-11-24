42e258dd883ff23859430cf07d7827de6e54615a
Andrzej Łuszcz <endriu1297@gmail.com>
2020-11-24 02:59:24 +0100

    add new functions edit user in admin panel

--- public/javascripts/adminInfo.js
+++ public/javascripts/adminInfo.js
@@ -1,5 +1,7 @@
 const infoBtn = document.querySelectorAll('.adminPanel__button--details');
+const editBtn = document.querySelectorAll('.adminPanel__button--edit');
 const deleteBtn = document.querySelectorAll('.adminPanel__button--delete');
+const submitEditBtn = document.querySelector('.submitEdit');
 const infoName = document.querySelector('.popUp__detailsInfo--name');
 const infoSurname = document.querySelector('.popUp__detailsInfo--surname');
 const infoLogin = document.querySelector('.popUp__detailsInfo--login');
@@ -8,6 +10,12 @@ const infoEmail = document.querySelector('.popUp__detailsInfo--email');
 const infoPhone = document.querySelector('.popUp__detailsInfo--phone');
 const infoDate = document.querySelector('.popUp__detailsInfo--date');
 const infoSex = document.querySelector('.popUp__detailsInfo--sex');
+const editName = document.querySelector('.input--name');
+const editLogin = document.querySelector('.input--login');
+const editSurname = document.querySelector('.input--surname');
+const editEmail = document.querySelector('.input--email');
+const editPhone = document.querySelector('.input--phone');
+let editId = 0;
 
 const setInfoData = (response) => {
   infoName.textContent = response.name;
@@ -20,8 +28,54 @@ const setInfoData = (response) => {
   infoSex.textContent = response.sex;
 };
 
+const setEditData = (response) => {
+  editName.value = response.name;
+  editSurname.value = response.surname;
+  editLogin.value = response.login;
+  editEmail.value = response.email;
+  editPhone.value = response.phone;
+};
+
 const deleteItem = (item) => {
   item.remove();
+};
+
+const dowloadData = () => {
+  const dataFormValue = [];
+  const formValueLength = document.forms[1].length;
+
+  for (let i = 0; i < formValueLength; i++) {
+    const item = document.forms[1][i];
+
+    if (item.classList.contains('signHolder__input')) {
+      dataFormValue.push(document.forms[1][i].value);
+    }
+  }
+  return dataFormValue;
+};
+
+const sendData = async (formData) => {
+  const url = `/admin/update/${editId}`;
+
+  const data = {
+    name: formData[1],
+    surname: formData[2],
+    email: formData[3],
+    phone: formData[4],
+    login: formData[0],
+  };
+
+  fetch(url, {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+    });
 };
 
 [...infoBtn].map((item) => {
@@ -56,4 +110,29 @@ const deleteItem = (item) => {
         deleteItem(parent);
       });
   });
+});
+
+[...editBtn].map((item) => {
+  item.addEventListener('click', () => {
+    const parent = item.parentElement;
+    const id = parent.dataset.id;
+    editId = id;
+
+    const url = `/admin/edit/${id}`;
+
+    fetch(url, {
+      method: 'get',
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        setEditData(response);
+      });
+  });
+});
+
+submitEditBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const formData = dowloadData();
+  sendData(formData);
 });
--- routes/admin.js
+++ routes/admin.js
@@ -23,6 +23,32 @@ router.get('/info/:id', function (req, res, next) {
   });
 });
 
+router.get('/edit/:id', function (req, res, next) {
+  const findUser = User.findOne({ _id: req.params.id });
+
+  findUser.exec((err, data) => {
+    res.json(data);
+  });
+});
+
+router.post('/update/:id', function (req, res, next) {
+  User.findOne({ _id: req.params.id }, (err, data) => {
+    if (err) {
+      res.json({ isUpdate: false });
+    }
+
+    data.name = req.body.name;
+    data.surname = req.body.surname;
+    data.email = req.body.email;
+    data.phone = req.body.phone;
+    data.login = req.body.login;
+
+    data.save((err) => {
+      res.json({ isUpdate: true });
+    });
+  });
+});
+
 router.get('/delete/:id', function (req, res, next) {
   const findUser = User.findByIdAndDelete({ _id: req.params.id });
 
