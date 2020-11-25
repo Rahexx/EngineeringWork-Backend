4ca50de4cf018b45709cdfabd242f71c167a91c8
Andrzej Łuszcz <endriu1297@gmail.com>
2020-11-25 14:56:50 +0100

    create new chat

--- routes/messages.js
+++ routes/messages.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const Message = require('../models/messages');
 
 router.all('*', (req, res, next) => {
   if (!req.session.role) {
@@ -13,6 +14,29 @@ router.all('*', (req, res, next) => {
 
 router.get('/', function (req, res, next) {
   res.render('messages', { role: req.session.role });
+});
+
+router.get('/:id', function (req, res, next) {
+  console.log(req.params.id);
+
+  const findMessage = Message.find({
+    idFrom: { $in: [req.params.id, req.session.id] },
+    idTo: { $in: [req.params.id, req.session.id] },
+  });
+
+  findMessage.exec((err, data) => {
+    if (data.length === 0) {
+      const newChat = new Message({
+        idFrom: req.session.id,
+        idTo: req.params.id,
+      });
+      newChat.save();
+
+      res.redirect('/messages');
+    } else {
+      res.redirect('/messages');
+    }
+  });
 });
 
 module.exports = router;
