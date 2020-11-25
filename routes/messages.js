1ae2a241917e24b24e30c6a2a47ffb281fb03825
Andrzej Łuszcz <endriu1297@gmail.com>
2020-11-25 13:50:44 +0100

    update navigation and add secure to route messages

--- routes/messages.js
+++ routes/messages.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+router.all('*', (req, res, next) => {
+  if (!req.session.role) {
+    res.redirect('/logIn');
+
+    return;
+  }
+
+  next();
+});
+
 router.get('/', function (req, res, next) {
-  res.render('messages');
+  res.render('messages', { role: req.session.role });
 });
 
 module.exports = router;
