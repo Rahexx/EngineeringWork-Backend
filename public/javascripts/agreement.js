3cc090c2aa437845f3b43f0726658f5c647869e6
Andrzej Łuszcz <endriu1297@gmail.com>
2020-11-30 22:31:21 +0100

    add to database new agreement

--- public/javascripts/agreement.js
+++ public/javascripts/agreement.js
@@ -1,19 +1,15 @@
 const agreementForm = document.querySelector('.addAgreement__form');
 const nameAgreement = document.querySelectorAll('.addAgreement__input')[0];
-const deadlineAgreemnt = document.querySelectorAll('.addAgreement__input')[1];
-const otherLogin = document.querySelectorAll('.addAgreement__input')[2];
+const otherLogin = document.querySelectorAll('.addAgreement__input')[1];
 const agreementFile = document.querySelector('.addAgreement__input--file');
 const submitBtn = document.querySelector('.addAgreement__submit');
 
 let validName = false;
-let validDeadline = false;
 let validLogin = false;
 
 const checkAllValid = () => {
-  if (validName && validDeadline && validLogin) {
-    console.log('Sprawdzam plik');
+  if (validName && validLogin) {
     if (agreementFile.value !== '') {
-      console.log('Jestem i wysyłam');
       submitBtn.disabled = false;
     }
   }
@@ -33,23 +29,7 @@ const validNameAgreement = () => {
   }
 };
 
-const validDeadlineAgreement = () => {
-  const deadlineError = document.querySelector('.addAgreement__errorDate');
-
-  if (deadlineAgreemnt.value === '') {
-    deadlineError.style.opacity = '1';
-    deadlineAgreemnt.style.border = '1px solid red';
-    validDeadline = false;
-  } else {
-    deadlineError.style.opacity = '0';
-    deadlineAgreemnt.style.border = '1px solid #adadad';
-    checkAllValid();
-    validDeadline = true;
-  }
-};
-
 nameAgreement.addEventListener('input', validNameAgreement);
-deadlineAgreemnt.addEventListener('input', validDeadlineAgreement);
 otherLogin.addEventListener('input', () => {
   if (otherLogin.value) {
     checkUser(otherLogin.value)
--- routes/account.js
+++ routes/account.js
@@ -130,22 +130,28 @@ router.post('/addSettlement/', function (req, res) {
 
 router.post('/addAgreement', (req, res) => {
   console.log(req.body);
-  console.log(req.files);
+
   if (req.files) {
     const file = req.files.file;
     const indexDot = file.name.indexOf('.');
     const name = file.name.slice(0, indexDot);
     const extendFile = file.name.slice(indexDot, file.name.length);
     const randomId = Math.floor(Math.random() * (1000000 - 10) + 10);
-    const fileName = `${name}.${randomId}${extendFile}`;
-
+    const fileName = `${req.body.name}.${randomId}${extendFile}`;
     const url = `${__dirname}/../public/agreements/${fileName}`;
 
     file.mv(url, (err) => {
       if (err) {
         console.log(err);
       } else {
-        console.log('Udało się');
+        const newAgreement = new Agreement({
+          tenantLogin: req.body.login,
+          ownerLogin: req.session.login,
+          pathFile: `/agreements/${fileName}`,
+        });
+
+        newAgreement.save();
+        res.redirect('/account');
       }
     });
   }
